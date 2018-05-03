6419661c3241004d55e6965c330f3a0b4ae14ab1
Alex Papanicolaou <alex.papanic@gmail.com>
2018-05-02 23:10:36 -0700

    js config

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+/* Start/end date of semester */
+var startDate = '2018-01-15', endDate = '2018-05-08';
+
+/* Calendar Specification */
+var apiKey = 'AIzaSyBb3qtBT4JXavAr4spAZNBayxU6Aa5lWz8';
+var ls88CalendarId = 'berkeley.edu_6gebkip878pubc6a4bajca1pqg@group.calendar.google.com';
